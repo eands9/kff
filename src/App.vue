c664a99ef6bc34944a047a1c3f75ab2d2b845a44
Eric Hernandez <erichernandez@Erics-MBP.home>
2022-09-05 22:35:11 -0500

    1st

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <app-header />
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+
+export default {
+  name: "App",
+  components: { AppHeader },
+};
+</script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,8 @@
+<template>
+  <header id="header" class="bg-white">
+    <nav class="container mx-auto flex justify-start items-center py-5 px-4">
+      <router-link to="/"><img src="leaf.jpg" class="h-10 w-10" /></router-link>
+      <router-link to="/"></router-link>
+    </nav>
+  </header>
+</template>
