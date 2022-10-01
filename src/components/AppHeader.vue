61b2cfd2f272609bfb05e714a98d0878e5c07772
Eric Hernandez <erichernandez@Erics-MBP.home>
2022-09-30 22:40:26 -0500

    fixed tailwind nav

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,9 @@
 <template>
   <header id="header" class="bg-black text-lime-600">
     <nav
-      class="p-2 flex justify-between items-center shadow md:flex md:items-center md:justify-between"
+      class="p-2 flex justify-between items-center shadow"
     >
-      <div class="">
+      <div class="z-[99]">
         <span class="font-[Cursive] cursor-pointer flex">
           <img class="h-10 inline" src="leafgreen3.png" />
 
@@ -18,19 +18,19 @@
         </span>
       </div>
 
-      <ul
-        class="md:flex md:items-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500"
-      >
+      <ul class="md:flex md:items-center md:z-auto md:static absolute
+     bg-black w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 
+     opacity-0 top-[-400px] transition-all ease-in duration-500"
+     :class="{'opacity-100 top-[50px] transition-all ease-in duration-500 z-[1]': isMobileNav}">
         <li class="mx-4 my-6 md:my-0">
-          <a href="#" class="hover:text-lime-500 duration-500">LOGIN</a>
+          <a href="#" class="hover:text-lime-300 duration-500">LOGIN</a>
         </li>
         <li class="mx-4 my-6 md:my-0">
-          <a href="#" class="hover:text-lime-500 duration-500">ACCOUNT</a>
+          <a href="#" class="hover:text-lime-300 duration-500">ACCOUNT</a>
         </li>
       </ul>
-
       <div class="flex">
-        <i class="pr-2 fas fa-short fa-solid fa-bars md:hidden"></i>
+        <i class="pr-2 fas fa-short fa-solid fa-bars md:hidden" @click="toggleBurger"></i>
         <span class="mr-4 flex relative cursor-pointer right-0">
           <i class="fas fa-shopping-cart" @click="toggleCart"></i>
           <span
@@ -69,18 +69,12 @@ export default {
   data() {
     return {
       cartQuantity: 1,
+      isMobileNav: false,
     };
   },
   methods: {
-    Menu(e) {
-      let list = document.querySelector("ul");
-      e.name === "menu"
-        ? ((e.name = "close"),
-          list.classList.add("top-[80px]"),
-          list.classList.add("opacity-100"))
-        : ((e.name = "menu"),
-          list.classList.remove("top-[80px]"),
-          list.classList.remove("opacity-100"));
+    toggleBurger(){
+      this.isMobileNav = !this.isMobileNav
     },
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen;
